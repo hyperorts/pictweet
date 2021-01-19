80dad9fe5f51913ccde80b82aed2d90b756bfb7d
hyperorts <loss.dealer@gmail.com>
2021-01-19 15:25:53 +0900

    編集機能実装
    editアクション定義
    routes.rbにedit追加
    index.html.erbの削除ラベルの上に編集ラベル設定
    tweets/controller.rbにedit追加
    編集画面のビュー作成：edit.html.erbファイル作成、中身記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,9 +17,12 @@ class TweetsController < ApplicationController
     tweet.destroy
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
-
 end
