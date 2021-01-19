0850451ddd4e1379dd3fc06cc860a1da7c6a8f00
hyperorts <loss.dealer@gmail.com>
2021-01-19 20:23:22 +0900

    ストロングパラメーターを設定
    devise_parameter_sanitizerメソッド
    deviseのctlにストロングパラメーター反映、devise特有のパラメーター取得
    ノートにメモ取った。（privateクラス外、appctlを使う理由等）
    viewでアマゾン梱包発送、ctlで開梱検品の流れを忘れずに。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+#許可されたパラメーターを構成する= before_action :configure_permitted_parameters,
+# if: :devise_controller?はその戻り値がtrueであったときにのみ処理を実行するよう設定するものです。
+#今回は:devise_controller?というdeviseのヘルパーメソッド名を指定して、もしdeviseに関するコントローラーの処理であれば、
+#そのときだけconfigure_permitted_parametersメソッドを実行するように設定しています。他のtweetsコントローラーなどでは処理は読み込まれても、実行まではされません。
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
