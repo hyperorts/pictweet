179869588b445150419b55643857d43b8289c554
hyperorts <loss.dealer@gmail.com>
2021-01-20 22:44:29 +0900

    投稿時の処理を変更する/tweetsテーブルから不要なカラムを削除しよう
    投稿時にnameの入力がいらなくなったので、tweetsコントローラーも編集する。nameカラムはもう使わないので、ツイート保存時にnameカラムへ情報を保存しないように編集する。
    rails g migration RemoveNameFromTweets name:stringでnameカラム削除、その後rails db:migrateで移行
    
    これによりusers/show.html.erbのtweet.nameでエラーが発生するのでusers/show.html.erbの一部記述を削除した。
    
    結果：マイページの投稿の名前が消えた。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,7 +38,7 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
 #↓の記述でbefore_actionと組み合わせる
   def set_tweet
